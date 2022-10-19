c2ea2d05721983d9eb771dc6e589549c82ddefc5
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2022-10-19 21:10:29 +0200

    Add files via upload

--- script/Flamvell/c210632002.lua
+++ script/Flamvell/c210632002.lua
@@ -34,9 +34,43 @@ end
 function s.costfilter(c)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsDiscardable()
 end
+function s.costfilter2(c,tp)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsMonster() and c:IsDefenseBelow(200) and c:IsAbleToGraveAsCost()
+end
 function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD)
+    local b1=Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler())
+    local b2=Duel.IsExistingMatchingCard(s.costfilter2,tp,LOCATION_DECK,0,1,nil,tp) and Duel.IsPlayerAffectedByEffect(tp,210632003)
+	if chk==0 then return b1 or b2 end
+    if b1 and b2 then
+        if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+            s.op(e,tp,eg,ep,ev,re,r,rp)
+        else
+            Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
+        end
+    elseif b1 and not b2 then
+        Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
+    else
+        s.op(e,tp,eg,ep,ev,re,r,rp)
+    end
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+    local fg=Group.CreateGroup()
+	for i,pe in ipairs({Duel.IsPlayerAffectedByEffect(tp,210632003)}) do
+		fg:AddCard(pe:GetHandler())
+	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local tc=Duel.SelectMatchingCard(tp,s.costfilter2,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+    if tc then
+        local fc=nil
+        if #fg==1 then
+            fc=fg:GetFirst()
+        else
+            fc=fg:Select(tp,1,1,nil)
+        end
+        Duel.Hint(HINT_CARD,0,fc:GetCode())
+        fc:RegisterFlagEffect(210632003,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+    end
+    Duel.SendtoGrave(tc,REASON_COST)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil) end
